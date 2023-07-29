30af598674a7f2d906d509788cb490a08868c699
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-07-29 14:20:40 +0530

    Session Added

--- src/lk/ijse/gdse66/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse66/hibernate/config/SessionFactoryConfig.java
@@ -1,5 +1,13 @@
 package lk.ijse.gdse66.hibernate.config;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 /**
  * @author : Kavithma Thushal
  * @project : ORM_GDSE66
@@ -12,12 +20,20 @@ public class SessionFactoryConfig {
     private SessionFactoryConfig() {
     }
 
-    public static SessionFactoryConfig getSessionFactoryConfig() {
+    public static SessionFactoryConfig getInstance() {
         /*if (sessionFactoryConfig == null) {
             return sessionFactoryConfig = new SessionFactoryConfig();
         }
         return sessionFactoryConfig;*/
 
         return (sessionFactoryConfig == null) ? sessionFactoryConfig = new SessionFactoryConfig() : sessionFactoryConfig;
+    }
+
+    public Session getSession() {
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+        Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE).build();
+        //SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        SessionFactory sessionFactory = metadata.buildSessionFactory();
+        return sessionFactory.openSession();
     }
 }

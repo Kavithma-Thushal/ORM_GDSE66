a389b9365b26fab9d5cd601900e81fae389ac52c
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-07-29 14:53:21 +0530

    Customer Table Added

--- src/lk/ijse/gdse66/hibernate/AppInitializer.java
+++ src/lk/ijse/gdse66/hibernate/AppInitializer.java
@@ -10,7 +10,6 @@ import org.hibernate.Session;
  **/
 public class AppInitializer {
     public static void main(String[] args) {
-
         Session session= SessionFactoryConfig.getInstance().getSession();
     }
 }

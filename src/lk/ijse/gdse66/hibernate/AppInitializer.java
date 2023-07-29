9ebb42814ca5ce548f5ec0c0cdc399ee6aee7be4
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-07-29 15:33:56 +0530

    Save Customer

--- src/lk/ijse/gdse66/hibernate/AppInitializer.java
+++ src/lk/ijse/gdse66/hibernate/AppInitializer.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse66.hibernate;
 
 import lk.ijse.gdse66.hibernate.config.SessionFactoryConfig;
+import lk.ijse.gdse66.hibernate.entity.Customer;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  * @author : Kavithma Thushal
@@ -10,7 +12,17 @@ import org.hibernate.Session;
  **/
 public class AppInitializer {
     public static void main(String[] args) {
-        Session session= SessionFactoryConfig.getInstance().getSession();
+        Session session = SessionFactoryConfig.getInstance().getSession();
 
+        Customer customer = new Customer();
+        customer.setId(1);
+        customer.setName("Thushal");
+        customer.setAddress("Galle");
+        customer.setSalary(23500.50);
+
+        Transaction transaction = session.beginTransaction();
+        session.save(customer);
+        transaction.commit();
+        session.close();
     }
 }

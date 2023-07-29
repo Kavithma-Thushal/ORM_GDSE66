f9e41f980ce4e20fdab35c3f5eb0e695c155d451
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-07-29 10:16:58 +0530

    Initial commit

---
+++ src/lk/ijse/gdse66/hibernate/AppInitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse66.hibernate;
+
+/**
+ * @author : Kavithma Thushal
+ * @project : ORM_GDSE66
+ * @since : 10:14 AM - 7/29/2023
+ **/
+public class AppInitializer {
+    public static void main(String[] args) {
+
+    }
+}

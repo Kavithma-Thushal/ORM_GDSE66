500f6ba32700538f6199a582a71ae03d7dca51d4
Kavithma Thushal <kavithmathushal9007@gmai.com>
2023-08-05 07:50:55 +0530

    XML Configuration Completed

--- src/lk/ijse/gdse66/hibernate/AppInitializer.java
+++ src/lk/ijse/gdse66/hibernate/AppInitializer.java
@@ -14,7 +14,7 @@ public class AppInitializer {
     public static void main(String[] args) {
 
         Customer customer = new Customer();
-        customer.setId(1);
+        customer.setId(4);
         customer.setName("Thushal");
         customer.setAddress("Galle");
         customer.setSalary(25000.00);
@@ -25,31 +25,5 @@ public class AppInitializer {
         saveSession.save(customer);     //persists (saves) customer object in DB
         saveTransaction.commit();
         saveSession.close();
-
-        //Update
-        Session updateSession = SessionFactoryConfig.getInstance().getSession();
-        Transaction updateTransaction = updateSession.beginTransaction();
-        updateSession.update(customer);
-        updateTransaction.commit();
-        updateSession.close();
-
-        //Delete
-        Session deleteSession = SessionFactoryConfig.getInstance().getSession();
-        Transaction deleteTransaction = deleteSession.beginTransaction();
-        Customer deleteCustomer = deleteSession.get(Customer.class, 1);
-        deleteSession.delete(deleteCustomer);       //deletes a Customer from DB
-        deleteTransaction.commit();
-        deleteSession.close();
-
-        /*//Get
-        Session updateSession = SessionFactoryConfig.getInstance().getSession();
-        Transaction updateTransaction = updateSession.beginTransaction();
-        Customer updateCustomer = updateSession.get(Customer.class, 1); // Retrieves (Get) customer data from DB by ID
-        updateCustomer.setName("Namal");
-        updateCustomer.setAddress("Hambantota");
-        //updateSession.save(customer1);    //you can use this to update an already existing data in DB
-        updateSession.update(updateCustomer);    //or you can use this to update an already existing data in DB
-        updateTransaction.commit();
-        updateSession.close();*/
     }
 }
